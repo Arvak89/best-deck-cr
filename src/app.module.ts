7ba453655b5dc2d604b400504dcc4e51c381ea91
Damian <soshnin_03@mail.ru>
2025-11-09 14:30:47 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+    imports: [ConfigModule.forRoot()],
+})
+export class AppModule {}
